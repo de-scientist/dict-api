369e6cb0101514faad4345fadf2fb4cd1f9b494b
de-scientist <gitaumark502@gmail.com>
2025-11-19 20:05:18 +0300

    import axios from "axios"; to use for fetching data from the api

--- src/lib/fetchDefinition.ts
+++ src/lib/fetchDefinition.ts
@@ -1,6 +1,12 @@
+import axios from "axios";
+
+
 export async function fetchDefinition(word: string) {
-  if (!word) return null;
-  const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-  if (!res.ok) throw new Error("Word not found");
-  return res.json();
+if (!word) return null;
+try {
+const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+return res.data;
+} catch (error) {
+throw new Error("Word not found");
+}
 }
